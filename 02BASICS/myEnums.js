bae917afe567021a836cac34aec6a6461825c0d6
Gayle Hoefer <gaylehoefer@gmail.com>
2023-03-24 14:05:17 -0700

    covered enums

---
+++ 02BASICS/myEnums.js
@@ -0,0 +1,12 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+// if (seat === 0)
+// enums limit the choice of options for the user
+// var SeatChoice;
+// (function (SeatChoice) {
+//     SeatChoice["AISLE"] = "AISLE";
+//     SeatChoice[SeatChoice["MIDDLE"] = 3] = "MIDDLE";
+//     SeatChoice[SeatChoice["WINDOW"] = 4] = "WINDOW";
+// })(SeatChoice || (SeatChoice = {}));
+// var hcSeat = SeatChoice.AISLE;
---
+++ 02BASICS/myEnums.ts
@@ -0,0 +1,13 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+
+// if (seat === 0)
+// enums limit the choice of options for the user
+enum SeatChoice {
+    AISLE = "AISLE",
+    MIDDLE = 3,
+    WINDOW 
+}
+
+const hcSeat = SeatChoice.AISLE
